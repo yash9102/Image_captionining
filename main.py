bb34d28f6dcad8b0787bd1e2f24d58e83d361740
yash9102 <87117100+yash9102@users.noreply.github.com>
2023-11-23 00:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def parse_agrs():
     parser.add_argument('--block_trigrams', type=int, default=1, help='whether to use block trigrams.')
 
     # Trainer settings
-    parser.add_argument('--n_gpu', type=int, default=2, help='the number of gpus to be used.')
+    parser.add_argument('--n_gpu', type=int, default=1, help='the number of gpus to be used.')
     parser.add_argument('--epochs', type=int, default=10, help='the number of training epochs.')
     parser.add_argument('--save_dir', type=str, default='results/iu_xray', help='the patch to save the models.')
     parser.add_argument('--record_dir', type=str, default='records/', help='the patch to save the results of experiments')
